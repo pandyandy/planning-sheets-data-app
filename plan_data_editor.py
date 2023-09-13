dc7862a8841f7cbe76b7120f300072519c617161
Monika Feigler <monika@feigler.cz>
2023-09-13 14:11:22 +0200

    update

--- plan_data_editor.py
+++ plan_data_editor.py
@@ -51,63 +51,50 @@ def get_dataframe(table_name):
     df = pd.read_csv('data.csv')
     return df
   
-with st.sidebar:
-    choose = option_menu("Data manipulation", ["Data-editor"],
-                         icons=['people', 'activity', 'person lines fill', 'activity'],
-                         menu_icon="app-indicator", default_index=0,
-                         styles={
-        "container": {"padding": "5!important", "background-color": "#FFFFFF"},
-        "icon": {"color": "#000000", "font-size": "25px"}, 
-        "nav-link": {"font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "grey"},
-        "nav-link-selected": {"background-color": "#0082fa"},
-    }
-    )
+
+def main():
+    
+    # Set up Streamlit container with title and logo
+    with st.container():
+        st.markdown(f"{logo_html}", unsafe_allow_html=True)
+        st.title("Interactive Keboola Sheets")
+    
+    client = Client(url, token)
+    tables = client.tables.list()
+    table_list = pd.DataFrame(tables)
+    st.markdown('Table list')
+    st.dataframe(table_list['id'])
+
+    # Get the unique values from a specific column. Replace 'column_name' with your actual column name.
+    unique_values = table_list['id'].unique()
+    # Add 'All' option to the unique values list
+    options = ['empty'] + list(unique_values)
+    # Create a select box with the unique values
+    
+    selected_value = st.selectbox('Select a value', options=options)
+    
+    # Filter the dataset based on the selected value
+    if selected_value == 'empty':
+        st.markdown('No table selected')
+    else:
+        data = get_dataframe(selected_value)
+    # Display the data in an editable table using st.data_editor
+        edited_data = st.data_editor(data, num_rows="dynamic", width=1400, height=500)
 
 
-if choose == "Data-editor":
-    def main():
-        
-        # Set up Streamlit container with title and logo
-        with st.container():
-            st.markdown(f"{logo_html}", unsafe_allow_html=True)
-            st.title("Interactive Keboola Sheets")
-        
-        client = Client(url, token)
-        tables = client.tables.list()
-        table_list = pd.DataFrame(tables)
-        st.markdown('Table list')
-        st.dataframe(table_list['id'])
-
-        # Get the unique values from a specific column. Replace 'column_name' with your actual column name.
-        unique_values = table_list['id'].unique()
-        # Add 'All' option to the unique values list
-        options = ['empty'] + list(unique_values)
-        # Create a select box with the unique values
-        
-        selected_value = st.selectbox('Select a value', options=options)
-        
-        # Filter the dataset based on the selected value
-        if selected_value == 'empty':
-            st.markdown('No table selected')
+    if st.button("Send to Keboola"):
+        if os.path.exists('updated_data.csv'):
+            os.remove('updated_data.csv.gz')
         else:
-            data = get_dataframe(selected_value)
-        # Display the data in an editable table using st.data_editor
-            edited_data = st.data_editor(data, num_rows="dynamic", width=1400, height=500)
-
-
-        if st.button("Send to Keboola"):
-            if os.path.exists('updated_data.csv'):
-                os.remove('updated_data.csv.gz')
-            else:
-                print("The file does not exist")
-            st.markdown(selected_value)
-            st.markdown('Updated!')
-            edited_data.to_csv('updated_data.csv.gz', index=False,compression='gzip')
-            
-            client_upload.tables.load(table_id = selected_value , file_path='updated_data.csv.gz', is_incremental=False)
+            print("The file does not exist")
+        st.markdown(selected_value)
+        st.markdown('Updated!')
+        edited_data.to_csv('updated_data.csv.gz', index=False,compression='gzip')
         
-        # Display HTML footer
-        st.markdown(html_footer, unsafe_allow_html=True)
+        client_upload.tables.load(table_id = selected_value , file_path='updated_data.csv.gz', is_incremental=False)
+    
+    # Display HTML footer
+    st.markdown(html_footer, unsafe_allow_html=True)
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
