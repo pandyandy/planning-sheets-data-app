a93528f87b1447a5b34d993a13ae09a8ba677110
Monika Feigler <monika@feigler.cz>
2023-09-13 14:28:05 +0200

    hide made with streamlit

--- plan_data_editor.py
+++ plan_data_editor.py
@@ -95,6 +95,13 @@ def main():
     
     # Display HTML footer
     st.markdown(html_footer, unsafe_allow_html=True)
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
